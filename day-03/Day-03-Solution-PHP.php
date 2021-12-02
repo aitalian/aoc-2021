59d498a21b523c7fd3cca6375a3250505d7ce4cd
Angelo Italiano <angelo.italiano@arctiq.ca>
2021-12-02 18:16:40 -0500

    Scaffolding Day 3

---
+++ day-03/Day-03-Solution-PHP.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Input data should be provided in `input.txt`.
+ * Example data rows for testing should be in `example-input.txt`.
+ * Tests will look for matching answers in `example-input-answers.txt` (one per line).
+ * Input data can also be provided from stdin.
+ *
+ * Example CLI usage:
+ * ```sh
+ *  # Read from STDIN
+ *  $ cat example-input.txt | php -f Day-03-Solution-PHP.php
+ *
+ *  # Read from default `input.txt`
+ *  $ php -f Day-03-Solution-PHP.php
+ *
+ *  # Run tests on example data
+ *  $ php -f Day-03-Solution-PHP.php test
+ * ```
+ */
+
+class ReadInputFile
+{
+    public $input = array();
+
+    public function FromSTDIN($fallbackFromFile = true)
+    {
+        $stdin = fopen('php://stdin', 'r');
+
+        if (is_resource($stdin)) {
+            stream_set_blocking($stdin, 0);
+
+            while (($f = fgets($stdin)) !== false) {
+                $this->input[] = trim($f);
+            }
+
+            fclose($stdin);
+        }
+
+        if (empty($this->input)) {
+            if ($fallbackFromFile) {
+                $this->FromFile('input.txt');
+            }
+        }
+
+        return $this;
+    }
+
+    public function FromFile($filename = 'input.txt')
+    {
+        if (file_exists($filename)) {
+            $this->input = array_filter(array_map('trim', explode("\n", file_get_contents($filename))));
+        }
+
+        return $this;
+    }
+
+    public function getInputArray()
+    {
+        return $this->input;
+    }
+}
+
+$testMode = false;
+
+if (isset($argv) && !empty($argv[1])) {
+    if ($argv[1] == "test") {
+        $testMode = true;
+    }
+}
+
+if ($testMode) {
+    $input = (new ReadInputFile)->FromFile('example-input.txt')->getInputArray();
+} else {
+    $input = (new ReadInputFile)->FromSTDIN(true)->getInputArray();
+}
+
+# ----- BEGIN Puzzle
+
+// Declare our answers
+$answers = array(
+    null,
+    null
+);
+
+print "Part One = ${answers[0]}\n";
+print "Part Two = ${answers[1]}\n";
+
+
+if ($testMode) {
+    $testAnswers = array_map('intval', (new ReadInputFile)->FromFile('example-input-answers.txt')->getInputArray());
+
+    print "\n\nTESTS\n=====\n";
+
+    for ($i = 0; $i < 2; $i++) {
+        print "\tPart " . ($i + 1) . ": " . ($answers[$i] === $testAnswers[$i] ? "PASSED" : "failed! Expected Value: ${testAnswers[$i]}") . "\n";
+    }
+}
